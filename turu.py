ff915ba1597539b88e38a1389a3b2c9426a86cfd
Gilberto M. Xavier <gilberto.xavier@lps.ufrj.br>
2023-11-11 13:57:00 -0300

    Update turu.py

--- turu.py
+++ turu.py
@@ -456,7 +456,7 @@ def cohen_coon(K, tau, theta,
     if type_of_controller == 'PID':
         Kp = (1/K)*((4/3)*(tau/theta) + 1/4)
         Ki = Kp/(theta*((32 + 6*(theta/tau))/(13 + 8*(theta/tau)))
-        Kd = Kp*(4*theta)/(11 + 2*(theta/tau))))
+        Kd = Kp*theta*(4/(11 + 2*(theta/tau)))
         return [Kp, Ki, Kd]    
 
 def fertik_sharpe(K, tau, theta, 
